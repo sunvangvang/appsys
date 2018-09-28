8fa9cfb31ff93bb5fc94588f41f9e41268600943
arvin <sunyy2012@sina.com>
2018-09-28 16:06:35 +0800

    Initial Project

---
+++ src/main/com/aibyd/appsys/App.java
@@ -0,0 +1,18 @@
+package com.aibyd.appsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by VSCODE
+ * User: Arven
+ * Date: 2018/09/28
+ * Time: 15:57
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
